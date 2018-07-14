403fadcebde9168ba4b9f630215d616095cb9702
root <yhyoo@rit.kaist.ac.kr>
2018-07-14 02:48:32 +0000

    new experiment

--- PCB/main.py
+++ PCB/main.py
@@ -247,9 +247,9 @@ if __name__=='__main__':
     parser.add_argument('--epochs', type=int, default=100) 
     parser.add_argument('--lr', type=float, default=1e-4) 
     parser.add_argument('--ksz', type=int, default=5) 
-    parser.add_argument('--nhid', type=list, default=[128,64,64]) 
-    parser.add_argument('--dropout', type=float, default=0.2) 
-    parser.add_argument('--h_dropout', type=float, default=0.0) 
+    parser.add_argument('--nhid', type=list, default=[64,64]) 
+    parser.add_argument('--dropout', type=float, default=0.3) 
+    parser.add_argument('--h_dropout', type=float, default=0.3) 
     parser.add_argument('--save_folder', type=str, default='image') 
 
     args = parser.parse_args() 
